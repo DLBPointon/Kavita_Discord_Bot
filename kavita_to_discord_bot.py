a04db05cc4fa804b63a4a88690098561947d0fd1
DLBPointon <damonlbp@hotmail.co.uk>
2023-06-20 09:53:20 +0100

    Script tested and uploads to discord

--- kavita_to_discord_bot.py
+++ kavita_to_discord_bot.py
@@ -11,51 +11,67 @@ Takes json list from Kavita, formats and posts to Discord.
 """
 
 from discord_webhook import DiscordWebhook, DiscordEmbed
-import json
+from dotenv import load_dotenv
 import os
 import requests
 import http.client
+import xml.etree.ElementTree as ET
 
-def args():
-    #kavita_api_key
-    #base_url
-    #kavita_port
-    #Discord hook
-    pass
+def dotloader():
+    load_dotenv()
+    kav_base = os.getenv('KAVITA_BASE')
+    kav_port = int(os.getenv('KAVITA_PORT'))
+    kav_api = os.getenv('KAVITA_API')
+    kav_jwt = os.getenv('KAVITA_JWT')
+    disc_hook = os.getenv('DISCORD_WEBHOOK')
+    return kav_base, kav_port, kav_api, kav_jwt, disc_hook
 
-def get_recommended_kavita_data(KAVITA_BASE: str, KAVITA_PORT: int, HEADERS):
-    KAVITA_EXTENTION = "/api/Recommended/quick-reads"
 
-    conn = http.client.HTTPConnection(KAVITA_BASE, KAVITA_PORT)
+def get_recommended_kavita_data(kav_base: str, kav_port: int, kav_headers):
+    EXTENTION = "/api/Recommended/quick-reads"
+
+    conn = http.client.HTTPConnection(kav_base, kav_port)
     payload = ''
-    conn.request("GET", KAVITA_EXTENTION, payload, HEADERS)
+    conn.request("GET", EXTENTION, payload, kav_headers)
     res = conn.getresponse()
     data = res.read()
-    return json.loads(data.decode("utf-8"))
+    return data.decode("utf-8")
 
-def get_recently_updated_kavita_data(KAVITA_BASE: str, KAVITA_PORT: int, HEADERS):
-    EXTENTION = "api/Opds/{KAVITA_API_KEY}/recently-added?pageNumber=1"
+def get_recently_updated_kavita_data(kav_base: str, kav_port: int, kav_api: str, kav_headers):
+    EXTENTION = f"api/Opds/{kav_api}/recently-added?pageNumber=1"
+    data = requests.get(f'http://{kav_base}:{kav_port}/{EXTENTION}')
+    return str_to_xml(data.content)
 
-    data = requests.get(f'http://{KAVITA_BASE}:{KAVITA_PORT}/{EXTENTION}')
-    return data.content
+def str_to_xml(data: str):
+    return ET.fromstring(data)
 
-def format_kavita_data(data):
-    for i in data:
-        print(i['name'])
+def get_titles(data: str):
+    title_list = ""
+    for x in data.iter('{http://www.w3.org/2005/Atom}entry'):
+        for i in x.findall('{http://www.w3.org/2005/Atom}title'):
+            title_list += f"{i.text.split(' (')[0]} \n"
+    return title_list
 
-def send_to_discord():
-    pass
+def send_to_discord(data, disc_hook):
+    content_pack = f'{data}'
+    print(data)
+    webhook = DiscordWebhook(url=disc_hook)
+    embed = DiscordEmbed(title='Todays Added Comics', description=content_pack)
+    
+    embed.set_timestamp()
+    embed.set_footer(text='I am currently a manually run bot')
+    webhook.add_embed(embed)
+    webhook.execute()
 
 def main():
-    KAVITA_BASE = "192.168.1.222"
-    KAVITA_PORT = 5000
-    HEADERS = {"Authorization": "Bearer {KAVITA_JWT}"}
-    DISCORD_WEBHOOK = ""
+    kav_base, kav_port, kav_api, kav_jwt, disc_hook = dotloader()
+    print(kav_base, kav_port, kav_api, kav_jwt, disc_hook)
+    kav_headers = {"Authorization": f"Bearer {kav_jwt}"}
 
-    data = get_recommended_kavita_data(KAVITA_BASE, KAVITA_PORT, HEADERS)
-    data2 = get_recently_updated_kavita_data(KAVITA_BASE, KAVITA_PORT, HEADERS)
-    #format_kavita_data(data)
-    print(data2)
+    data = get_recommended_kavita_data(kav_base, kav_port, kav_headers)
+    data2 = get_recently_updated_kavita_data(kav_base, kav_port, kav_api, kav_headers)
+    list = get_titles(data2)
+    send_to_discord(list, disc_hook)
     
 if __name__ == '__main__':
     main()
